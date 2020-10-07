cb8d39fd24b7de0a727d30aa265c99a281944e0b
BreadDeveloper <35596943+BreadDeveloper@users.noreply.github.com>
2020-10-07 20:19:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OpenWeatherMapAPIPractice
+ This is what came of my practice with the OpenWeatherMap public API
---
+++ script.js
@@ -0,0 +1,18 @@
+var city = "Sheffield"; // Enter a city to see it's weather information
+
+$.getJSON(
+	"https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=226eaa11a5bb05f34d0c2e36766cc55f",
+	function (data) {
+		var locationName = data.name;
+		var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+		var temp = Math.floor(data.main.temp);
+		var weather = data.weather[0].main;
+
+		document.title = "Weather in " + locationName; // change page title
+
+		$(".location-name").append(locationName);
+		$(".icon").attr("src", icon); // draw weather icon
+		$(".temp").append(temp + "°C"); // add temperature text
+		$(".weather-type").append(weather); // add weather type text
+	}
+);
